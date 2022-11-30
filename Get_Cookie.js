d62b637a559d7f4117e3c686ef989e66ab03a523
vinoth9600 <118962988+vinoth9600@users.noreply.github.com>
2022-11-30 17:16:35 +0530

    Update and rename Get.js to Get_Cookie.js

--- Get_Cookie.js
+++ Get_Cookie.js
@@ -1,12 +1,14 @@
-            function ReadCookie() {
+function ReadCookie() 
+{
                var allcookies = document.cookie;
                document.write ("All Cookies : " + allcookies );
                // Get all the cookies pairs in an array
                cookiearray = allcookies.split(';');
                // Now take key value pair out of this array
-               for(var i=0; i<cookiearray.length; i++) {
+               for(var i=0; i<cookiearray.length; i++) 
+               {
                   name = cookiearray[i].split('=')[0];
                   value = cookiearray[i].split('=')[1];
                   document.write ("Key is : " + name + " and Value is : " + value);
                }
-            }
+}
