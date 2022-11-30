5979e73291cd3ee3476f062d4b5ac29dbe391357
vinoth9600 <118962988+vinoth9600@users.noreply.github.com>
2022-11-30 16:39:55 +0530

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -3,14 +3,24 @@ function setCookie()
         document.cookie="username=Vinoth Kumar";  
     }  
     function getCookie()  
-    {  
-        if(document.cookie.length!=0)  
-        {  
-            var array=document.cookie.split("=");  
-        alert("Name="+array[0]+" "+"Value="+array[1]);  
-        }  
-        else  
-        {  
-        alert("Cookie not available");  
-        }  
-    }  
+    {
+    var cookies = { };
+
+    if (document.cookie && document.cookie != ”) 
+        {
+        var split = document.cookie.split(‘;’);
+            for (var i = 0; i < split.length; i++) 
+            {
+            var name_value = split[i].split(“=”);
+            name_value[0] = name_value[0].replace(/^ /, ”);
+            cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
+            }
+        }
+    return cookies;   
+    }
+
+    var cookies = get_cookies_array();
+    for(var name in cookies) 
+    {
+    console.log( name + ” : ” + cookies[name] + “” );
+    }
