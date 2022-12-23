dfee674db4c332c24befa72fd7de0f5f78621292
vinoth9600 <118962988+vinoth9600@users.noreply.github.com>
2022-12-23 22:46:20 +0530

    Add files via upload

---
+++ Consent_CSP.js
@@ -0,0 +1,10 @@
+
+<!-- After adding this script, call setConsentBannerParams({uuid: <unique id> }) to set unique ID for a customer. -->
+<script
+	defer
+	data-strict-csp
+	src="https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-strict-csp.js"
+	data-tenant-uuid="fc7a1c8b-281a-461f-8245-d27a7164f69b"
+	data-domain-uuid="58627d55-d641-4fc2-b6c4-8cf13ea442ea"
+	data-backend-url="https://app.securiti.ai">
+</script>
